bd00aae7a4471204c2b7010bc41fb8e2b45c59b4
Wahyu Kwan <wahyukwan@gmail.com>
2025-10-30 11:36:36 +0700

    Initial Commit

---
+++ Pawfect Guide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pawfect Guide
+//
+//  Created by Moladin on 30/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pawfect Guide/Pawfect_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pawfect_GuideApp.swift
+//  Pawfect Guide
+//
+//  Created by Moladin on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pawfect_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
